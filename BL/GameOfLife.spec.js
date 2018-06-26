e4008796445e37c41565c1e2974c86bbd0045418
Misha <mishaled@gmail.com>
2018-06-26 21:48:14 +0300

    test nextState conditions

--- BL/GameOfLife.spec.js
+++ BL/GameOfLife.spec.js
@@ -16,6 +16,128 @@ describe('GameOfLife', function () {
             chai_1.expect(actualBoard).to.be.deep.equal(expectedBoard);
         });
     });
+    describe('ComputeNextState', function () {
+        var computeNextStateBaseCheck = function (matrix, expected) {
+            var expectedBoard = new Model_1.Board(matrix);
+            var game = new _1.GameOfLife(expectedBoard);
+            var nextBoard = game.ComputeNextState();
+            var cell = new Model_1.Cell(1, 1);
+            var currentState = nextBoard.GetCurrentCellState(cell);
+            chai_1.expect(currentState).to.be.deep.equal(expected);
+        };
+        describe('Living cell', function () {
+            it('with 0 living neighbors - should die', function () {
+                var matrix = [[false, false, false],
+                    [false, true, false],
+                    [false, false, false]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 1 living neighbor - should die', function () {
+                var matrix = [[false, false, false],
+                    [true, true, false],
+                    [false, false, false]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 2 living neighbors - should live', function () {
+                var matrix = [[true, true, false],
+                    [false, true, false],
+                    [false, false, false]];
+                computeNextStateBaseCheck(matrix, true);
+            });
+            it('with 3 living neighbors - should live', function () {
+                var matrix = [[true, true, false],
+                    [true, true, false],
+                    [false, false, false]];
+                computeNextStateBaseCheck(matrix, true);
+            });
+            it('with 4 living neighbors - should die', function () {
+                var matrix = [[true, true, false],
+                    [true, true, false],
+                    [true, false, false]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 5 living neighbors - should die', function () {
+                var matrix = [[true, true, false],
+                    [true, true, false],
+                    [true, true, false]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 6 living neighbors - should die', function () {
+                var matrix = [[true, true, false],
+                    [true, true, false],
+                    [true, true, true]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 7 living neighbors - should die', function () {
+                var matrix = [[true, true, false],
+                    [true, true, true],
+                    [true, true, true]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 8 living neighbors - should die', function () {
+                var matrix = [[true, true, true],
+                    [true, true, true],
+                    [true, true, true]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+        });
+        describe('Dead cell', function () {
+            it('with 0 living neighbors - should die', function () {
+                var matrix = [[false, false, false],
+                    [false, false, false],
+                    [false, false, false]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 1 living neighbor - should die', function () {
+                var matrix = [[false, false, false],
+                    [true, false, false],
+                    [false, false, false]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 2 living neighbors - should die', function () {
+                var matrix = [[true, true, false],
+                    [false, false, false],
+                    [false, false, false]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 3 living neighbors - should live', function () {
+                var matrix = [[true, true, false],
+                    [true, false, false],
+                    [false, false, false]];
+                computeNextStateBaseCheck(matrix, true);
+            });
+            it('with 4 living neighbors - should die', function () {
+                var matrix = [[true, true, false],
+                    [true, false, false],
+                    [true, false, false]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 5 living neighbors - should die', function () {
+                var matrix = [[true, true, false],
+                    [true, false, false],
+                    [true, true, false]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 6 living neighbors - should die', function () {
+                var matrix = [[true, true, false],
+                    [true, false, false],
+                    [true, true, true]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 7 living neighbors - should die', function () {
+                var matrix = [[true, true, false],
+                    [true, false, true],
+                    [true, true, true]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+            it('with 8 living neighbors - should die', function () {
+                var matrix = [[true, true, true],
+                    [true, false, true],
+                    [true, true, true]];
+                computeNextStateBaseCheck(matrix, false);
+            });
+        });
+    });
 });
 
 //# sourceMappingURL=GameOfLife.spec.js.map
--- BL/GameOfLife.spec.ts
+++ BL/GameOfLife.spec.ts
@@ -19,4 +19,185 @@ describe('GameOfLife', () => {
             expect(actualBoard).to.be.deep.equal(expectedBoard);
         });
     });
+
+    describe('ComputeNextState', () => {
+
+        let computeNextStateBaseCheck = function (matrix: boolean[][], expected: boolean) {
+            let expectedBoard = new Board(matrix);
+
+            let game = new GameOfLife(expectedBoard);
+            let nextBoard = game.ComputeNextState();
+
+            let cell = new Cell(1, 1);
+            let currentState = nextBoard.GetCurrentCellState(cell);
+
+            expect(currentState).to.be.deep.equal(expected);
+        }
+
+        describe('Living cell', () => {
+            it('with 0 living neighbors - should die', () => {
+                let matrix =
+                    [[false, false, false],
+                    [false, true, false],
+                    [false, false, false]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+            
+            it('with 1 living neighbor - should die', () => {
+                let matrix =
+                    [[false, false, false],
+                    [true, true, false],
+                    [false, false, false]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 2 living neighbors - should live', () => {
+                let matrix =
+                    [[true, true, false],
+                    [false, true, false],
+                    [false, false, false]];
+
+                computeNextStateBaseCheck(matrix, true);
+            });
+
+            it('with 3 living neighbors - should live', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, true, false],
+                    [false, false, false]];
+
+                computeNextStateBaseCheck(matrix, true);
+            });
+
+            it('with 4 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, true, false],
+                    [true, false, false]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 5 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, true, false],
+                    [true, true, false]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 6 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, true, false],
+                    [true, true, true]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 7 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, true, true],
+                    [true, true, true]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 8 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, true],
+                    [true, true, true],
+                    [true, true, true]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+        });
+
+        describe('Dead cell', () => {
+            it('with 0 living neighbors - should die', () => {
+                let matrix =
+                    [[false, false, false],
+                    [false, false, false],
+                    [false, false, false]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 1 living neighbor - should die', () => {
+                let matrix =
+                    [[false, false, false],
+                    [true, false, false],
+                    [false, false, false]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 2 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, false],
+                    [false, false, false],
+                    [false, false, false]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 3 living neighbors - should live', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, false, false],
+                    [false, false, false]];
+
+                computeNextStateBaseCheck(matrix, true);
+            });
+
+            it('with 4 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, false, false],
+                    [true, false, false]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 5 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, false, false],
+                    [true, true, false]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 6 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, false, false],
+                    [true, true, true]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 7 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, false],
+                    [true, false, true],
+                    [true, true, true]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+
+            it('with 8 living neighbors - should die', () => {
+                let matrix =
+                    [[true, true, true],
+                    [true, false, true],
+                    [true, true, true]];
+
+                computeNextStateBaseCheck(matrix, false);
+            });
+        });
+    });
 });
